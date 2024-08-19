11a20145ff65ed0d0bd42aacb05265705647a2b6
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2024-08-19 11:25:19 -0400

    Saludar por genero

--- saludo_nombre.js
+++ saludo_nombre.js
@@ -7,4 +7,23 @@ function saludar() {
 
     // Mostrar el saludo en el párrafo con id "saludo"
     document.getElementById("saludo").innerText = mensaje;
+
+     // Obtener el género seleccionado
+     var genero = document.getElementById("genero").value;
+    
+     // Generar el saludo basado en el género
+     var saludoGenero;
+     if (genero === "masculino") {
+         saludoGenero = "Bienvenido";
+     } else if (genero === "femenino") {
+         saludoGenero = "Bienvenida";
+     } else {
+         saludoGenero = "Bienvenide"; // Inclusivo para otro género
+     }
+     
+     // Crear el mensaje de saludo
+     var mensaje = saludoGenero + ", " + nombre + "!";
+     
+     // Mostrar el saludo en el párrafo con id "saludo"
+     document.getElementById("saludo").innerText = mensaje;
 }
