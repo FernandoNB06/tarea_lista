beb547ed9f14771be42b09486306744c76d7c357
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2024-08-19 11:31:13 -0400

    Saludar por edad

--- saludo_nombre.js
+++ saludo_nombre.js
@@ -2,28 +2,25 @@ function saludar() {
     // Obtener el nombre del input
     var nombre = document.getElementById("nombre").value;
     
-    // Generar el saludo
-    var mensaje = "¡Hola, " + nombre + "!";
-
+    // Obtener la edad del input
+    var edad = document.getElementById("edad").value;
+    
+    // Obtener el género seleccionado
+    var genero = document.getElementById("genero").value;
+    
+    // Generar el saludo basado en el género
+    var saludoGenero;
+    if (genero === "masculino") {
+        saludoGenero = "Bienvenido";
+    } else if (genero === "femenino") {
+        saludoGenero = "Bienvenida";
+    } else {
+        saludoGenero = "Bienvenide"; // Inclusivo para otro género
+    }
+    
+    // Crear el mensaje de saludo con nombre, género y edad
+    var mensaje = saludoGenero + ", " + nombre + ". Tienes " + edad + " años.";
+    
     // Mostrar el saludo en el párrafo con id "saludo"
     document.getElementById("saludo").innerText = mensaje;
-
-     // Obtener el género seleccionado
-     var genero = document.getElementById("genero").value;
-    
-     // Generar el saludo basado en el género
-     var saludoGenero;
-     if (genero === "masculino") {
-         saludoGenero = "Bienvenido";
-     } else if (genero === "femenino") {
-         saludoGenero = "Bienvenida";
-     } else {
-         saludoGenero = "Bienvenide"; // Inclusivo para otro género
-     }
-     
-     // Crear el mensaje de saludo
-     var mensaje = saludoGenero + ", " + nombre + "!";
-     
-     // Mostrar el saludo en el párrafo con id "saludo"
-     document.getElementById("saludo").innerText = mensaje;
 }
